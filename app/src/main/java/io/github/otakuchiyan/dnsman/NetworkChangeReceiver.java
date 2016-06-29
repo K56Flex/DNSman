5d0edac3d293e1c5b8fee52f2f9ca96274163f62
ayman abdelghany <aymana.ghany@devfactory.com>
2016-06-29 13:52:50 +0200

    [squid:S1066] Merging collapsible if statements increases the code's readability

--- app/src/main/java/io/github/otakuchiyan/dnsman/NetworkChangeReceiver.java
+++ app/src/main/java/io/github/otakuchiyan/dnsman/NetworkChangeReceiver.java
@@ -19,23 +19,20 @@ public class NetworkChangeReceiver extends BroadcastReceiver implements ValueCon
         NetworkInfo currentNet;
 
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
-        if (isFirstConnect && sp.getBoolean(ValueConstants.KEY_PREF_AUTO_SETTING, true)) {
+        if (isFirstConnect && sp.getBoolean(ValueConstants.KEY_PREF_AUTO_SETTING, true) 
+                && !sp.getBoolean(ValueConstants.KEY_FIRST_BOOT, true)) {
             //Not first boot
-            if (!sp.getBoolean(ValueConstants.KEY_FIRST_BOOT, true)) {
-
-                //Workaround to deal with multiple broadcast
-                cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-                currentNet = cm.getActiveNetworkInfo();
-                if (currentNet != null) {
-                    isFirstConnect = false;
-                    String dnsToast = sp.getString(ValueConstants.KEY_PREF_TOAST, ValueConstants.TOAST_SHOW);
-                    BackupNetworkDnsTask.startAction(context);
-                    LocalBroadcastManager.getInstance(context).sendBroadcast(new Intent(ACTION_NETWORK_CONNECTED));
-                    if (!ExecuteIntentService.startActionByInfo(context, currentNet)) {
-                        if (!dnsToast.equals(ValueConstants.TOAST_NEVER)) {
-                            Toast.makeText(context, R.string.toast_no_dns, Toast.LENGTH_LONG).show();
-                        }
-                    }
+            //Workaround to deal with multiple broadcast
+            cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            currentNet = cm.getActiveNetworkInfo();
+            if (currentNet != null) {
+                isFirstConnect = false;
+                String dnsToast = sp.getString(ValueConstants.KEY_PREF_TOAST, ValueConstants.TOAST_SHOW);
+                BackupNetworkDnsTask.startAction(context);
+                LocalBroadcastManager.getInstance(context).sendBroadcast(new Intent(ACTION_NETWORK_CONNECTED));
+                if (!ExecuteIntentService.startActionByInfo(context, currentNet) 
+                        && !dnsToast.equals(ValueConstants.TOAST_NEVER)) {
+                    Toast.makeText(context, R.string.toast_no_dns, Toast.LENGTH_LONG).show();
                 }
             }
         }
